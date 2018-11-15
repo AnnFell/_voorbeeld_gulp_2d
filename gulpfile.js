78487990292b8f221140bd7efc993e09d4750d9b
AnnFell <annelottef@gmail.com>
2018-11-15 12:12:38 +0100

    browsersync + sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
-const gulp = require('gulp');
+var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['scss'], function() {
+
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch('./gulp/scss/**/*.scss', ['scss']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
 gulp.task('scss', function(){
   return gulp.src('./gulp/scss/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('/map'))
     .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
@@ -14,4 +30,4 @@ gulp.task('watch', function () {
 });
 
 
-gulp.task('default',['scss', 'watch']);
+gulp.task('default',['scss', 'serve']);
